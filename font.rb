4ab6e6b0e027f5cc68db90851ab67316f896ce06
aa <aa@localhost.(none)>
2009-06-27 07:43:30 -0700

    font preview

---
+++ font.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'pp'
+require 'RMagick'
+
+Rows = 1500
+Cols = 800
+
+fonts = Dir['/Library/Fonts/*']
+ex = Magick::Image.new(Cols, Rows)
+
+text = Magick::Draw.new
+text.pointsize = 36
+
+y=30;
+fonts.each do |font|
+  next if font.index('fonts.cache')
+  next if font.index('Apple')
+  text.font = '/Library/Fonts/Arial' 
+  text.annotate(ex, 0,0,320,y, font[14..-1]) do 
+    self.fill = 'maroon'
+  end
+  text.font = font 
+  text.annotate(ex, 0,0,-1,y, ARGV[0]) do 
+    self.fill = 'maroon'
+  end
+  y += 30
+end
+
+ex.write('drop_shadow.png')
+exit
