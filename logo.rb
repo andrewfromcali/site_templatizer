75b65e1ac0f0c13b94b85034e4a8a23a45661ea7
aa <aa@localhost.(none)>
2009-06-28 08:27:35 -0700

    logo

--- logo.rb
+++ logo.rb
@@ -4,6 +4,30 @@ require 'RMagick'
 
 class Logo
 
+  def render
+    draw_word(@word1, 10, 30, 'white')
+    draw_word(@word2, @met1.width+12, 30, '#FF9807')
+
+    mask = Magick::Image.new(@image.columns, 20)
+    gc = Magick::Draw.new
+    gc.composite(0, 0, 0, 0, @image.crop(0,10, @image.columns, 30))
+    gc.draw(mask)
+
+    gc = Magick::Draw.new
+    gc.composite(0, 30, 0, 0, mask.rotate(180).flop)
+    gc.draw(@image)
+    @image.write('logo.png')
+  end
+
+  def draw_word(word, x, y, color)
+    gc = Magick::Draw.new
+    gc.font = @font
+    gc.pointsize = 24 
+    gc.fill = color
+    gc.text(x,y,word)
+    gc.draw(@image)
+  end
+
   def self.run
     logo = Logo.new(ARGV[0], ARGV[1], ARGV[2])
     logo.render
@@ -21,31 +45,13 @@ class Logo
     @met1 = gc.get_type_metrics(@word1)
     @met2 = gc.get_type_metrics(@word2)
     cols = @met1.width+@met2.width+30
-    rows = @met1.height+20
+    rows = @met1.height+40
     @image = Magick::Image.new(cols, rows) do
       self.background_color = '#282828'
     end
   end
 
-  def draw_word(word)
-    gc = Magick::Draw.new
-    gc.font = @font
-    gc.pointsize = 24 
-    gc.fill = 'white'
-    gc.text(10,30,word)
-    gc.draw(@image)
-  end
-
-  def render
-    draw_word(@word1)
-    @image.write('logo.png')
-  end
-
 end
 
 Logo.run
-
-
-#gc.fill = '#FF9807'
-#gc.text(word1.width+12,30,ARGV[2])
 
