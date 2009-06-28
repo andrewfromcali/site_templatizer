10e2a98827d339aa15a6cc4395f6c7794689e173
aa <aa@localhost.(none)>
2009-06-28 07:27:47 -0700

    size

--- logo.rb
+++ logo.rb
@@ -2,31 +2,26 @@ require 'rubygems'
 require 'pp'
 require 'RMagick'
 
-Rows = 50
-Cols = 220
-
 fonts = Dir['/Library/Fonts/*']
-ex = Magick::Image.new(Cols, Rows) do
+gc = Magick::Draw.new
+gc.font = fonts[ARGV[0].to_i] 
+gc.pointsize = 24 
+word1 = gc.get_type_metrics(ARGV[1])
+word2 = gc.get_type_metrics(ARGV[2])
+cols = word1.width+word2.width+30
+rows = word1.height+20
+
+ex = Magick::Image.new(cols, rows) do
   self.background_color = '#282828'
 end
 
 gc = Magick::Draw.new
 gc.font = fonts[ARGV[0].to_i] 
 gc.pointsize = 24 
-#gc.stroke('white')
-gc.fill('white')
-gc.text(30,30, ARGV[1])
-
+gc.fill = 'white'
+gc.text(10,30,ARGV[1])
+gc.text(word1.width+12,30,ARGV[2])
 gc.draw(ex)
-
-#text.pointsize = 36
-#text.font = fonts[ARGV[0].to_i] 
-#text.annotate(ex, 0,0,10,40, ARGV[1]) do 
-#  self.fill = 'green'
-#end
-#text.annotate(ex, 0,0,10,40, ARGV[1]) do 
-#  self.fill = 'green'
-#end
 
 ex.write('logo.png')
 exit
