f012d8f8a118e2bb72f12bde201dad690c504bb9
aa <aa@localhost.(none)>
2009-06-28 15:12:33 -0700

    r

--- logo.rb
+++ logo.rb
@@ -4,50 +4,34 @@ require 'RMagick'
 
 class Logo
 
-  def grad(color, w, h)
-    grad = Magick::Image.read("gradient:#{color}") do
-      self.size = "#{w}x#{h}"
-    end
-    grad.first
-  end
-
-  def do_pattern(name, w, h, grad, color, image)
-    gc = Magick::Draw.new
-    gc.pattern(name, 0, 0, w, h) do
-      gc.composite(0, 0, 0, 0, grad)
-    end
-    gc.fill(color)
-    gc.stroke(name)
-    gc.rectangle(0, 0, w, h)
-    gc.draw(image)
+  def new_pix(p, y)
+    Magick::Pixel.new(p.red / 2, p.green / 2 , p.blue / 2, 0)
   end
 
   def render
-    mirror = Magick::Image.new(@met1.width+@met2.width+15, @met1.height) do
+    base = Magick::Image.new(@met1.width+@met2.width+15, @met1.height) do
       self.background_color = 'none'
     end
-    w = mirror.columns
-    h = mirror.rows
-    draw_word(@word1, 10, 20, 'white', mirror)
-    draw_word(@word2, @met1.width+12, 20, '#FF9807', mirror)
+    w = base.columns
+    h = base.rows
+    draw_word(@word1, 10, 20, 'white', base)
+    draw_word(@word2, @met1.width+12, 20, '#FF9807', base)
+
+    mirror = base.rotate(180).flop.crop(0, 0, w, h * 0.65)
+    mirror.each_pixel do |p, x, y|
+
+      if p.red != 0 and p.green != 0 and p.blue != 0
+        p = new_pix(p, y)
+      end
+      mirror.store_pixels(x, y, 1, 1, [p])
+    end
+
     gc = Magick::Draw.new
-    gc.composite(0, 10, 0, 0, mirror)
+    gc.composite(0, 30, 0, 0, mirror)
     gc.draw(@image)
 
-    mirror = mirror.rotate(180).flop
-
-    g1 = grad('black-white', mirror.columns, mirror.rows)
-    g2 = grad('#ffffffcf-#ffffff10', mirror.columns, mirror.rows)
-
-    mask1 = Magick::Image.new(w, h)
-    do_pattern('p1', w, h, g1, 'white', mask1)
-    mask1.alpha(Magick::DeactivateAlphaChannel)
-    mirror.alpha(Magick::ActivateAlphaChannel)
-    rounded = mirror.composite(mask1, Magick::CenterGravity, Magick::CopyOpacityCompositeOp)
-    do_pattern('p2', w, h, g2, nil, rounded)
-    
     gc = Magick::Draw.new
-    gc.composite(0, 33, 0, 0, rounded)
+    gc.composite(0, 16, 0, 0, base)
     gc.draw(@image)
     @image.write('logo.png')
   end
@@ -57,6 +41,7 @@ class Logo
     gc.font = @font
     gc.pointsize = 24 
     gc.fill = color
+    gc.stroke = 'transparent'
     gc.text(x,y,word)
     gc.draw(image)
   end
