58341fc128d0b8b0d49952eae3cecc73851fff09
aa <aa@localhost.(none)>
2009-07-06 01:28:35 -0700

    fix

--- scripts/adbox.rb
+++ scripts/adbox.rb
@@ -3,13 +3,13 @@ require 'pp'
 require 'RMagick'
 include Magick
 
-light = '#FF7E00'
-med = 'orange'
-dark = '#D2691E'
-
 light = '#248A95'
 med = '#07484E'
 dark = '#053D44'
+
+light = '#958A24'
+med = '#4E4807'
+dark = '#443D05'
 
 image = Image.new(212, 159)
 
@@ -46,14 +46,16 @@ gc.stroke_width(3)
 gc.roundrectangle(1,1,210,155, 5, 5)
 gc.draw(final)
 
-def t(txt, size, font, x, y, image)
-  gc = Magick::Draw.new
-  gc.pointsize = size 
-  gc.font = font
-  gc.fill = 'black'
-  gc.stroke = 'none'
-  gc.text(x+2,y+2,txt)
-  gc.draw(image)
+def t(txt, size, font, x, y, image, drop=true)
+  if drop
+    gc = Magick::Draw.new
+    gc.pointsize = size 
+    gc.font = font
+    gc.fill = 'black'
+    gc.stroke = 'none'
+    gc.text(x+2,y+2,txt)
+    gc.draw(image)
+  end
   gc = Magick::Draw.new
   gc.font = font 
   gc.pointsize = size 
@@ -64,24 +66,29 @@ def t(txt, size, font, x, y, image)
 end
 
 f1 = '/Library/Fonts/Arial Rounded Bold'
-t('Get Story Coverage', 18, f1, 14, 29, final)
+#t('Get Story Coverage', 18, f1, 14, 29, final)
+t('Actor Headshots', 18, f1, 14, 29, final)
+
+f2 = 'Veranda'
+#t('A professional review of', 14, f2, 14, 51, final, false)
+#t('your novel or screenplay', 14, f2, 14, 65, final, false)
+t('Latest digital camera to', 14, f2, 14, 51, final, false)
+t('capture the look you want', 14, f2, 14, 65, final, false)
+
+#t('$99', 28, f1, 20, 105, final)
+#t('Thorough 3 page', 14, f2, 80, 92, final)
+#t('review!', 14, f2, 80, 107, final)
+t('$80', 28, f1, 20, 105, final)
+t('Two hour shoot', 14, f2, 80, 92, final)
+t('See proofs online', 14, f2, 80, 107, final)
 
 gc = Magick::Draw.new
-gc.font = 'Veranda'
-gc.pointsize = 14 
-gc.fill = 'white'
-gc.stroke = 'none'
-gc.text(14,51,'A professional review of')
+gc.fill = 'none'
+gc.stroke = light 
+gc.fill = dark
+gc.rectangle(11, 123, 125, 146)
 gc.draw(final)
 
-gc = Magick::Draw.new
-gc.font = 'Veranda'
-gc.pointsize = 14 
-gc.fill = 'white'
-gc.stroke = 'none'
-gc.text(14,65,'your novel or screenplay')
-gc.draw(final)
+t('LEARN MORE >>', 12, f2, 20, 140, final, false)
 
-t('$99', 28, f1, 22, 107, final)
-
-final.write('../site1/images/ad.png')
+final.write('../site1/images/ad2.png')
